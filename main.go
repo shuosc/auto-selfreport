f853259a4d2ccd15c432d383038849c440038c00
Siyuan Zhang | 张豆 <kastnerorz@gmail.com>
2020-02-28 14:10:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func login(username, password string) {
 }
 
 func sendMail(to, content string) (err error) {
-	if len(strings.trimSpace(to)) == 0{
+	if len(strings.TrimSpace(to)) == 0{
 		return
 	}
 	inf := map[string]string{
@@ -133,6 +133,7 @@ func getViewParam(body io.Reader) map[string]string {
 	xxMatch := regexp.MustCompile(`f13_state={.+?"Text":"(.+?)"`).FindStringSubmatch(html)
 	jcMatch := regexp.MustCompile(`f14_state={.+?"SelectedValueArray":\["(.+?)"]`).FindStringSubmatch(html)
 	ssMatch := regexp.MustCompile(`f32_state={.+?"SelectedValue":"(.+?)"`).FindStringSubmatch(html)
+	date := time.Now().Format("2006-01-02")
 	var F_State string
 	var shanghai bool
 	if (len(tzMatch) < 2 || len(ssMatch) < 2) {
@@ -142,7 +143,6 @@ func getViewParam(body io.Reader) map[string]string {
 		shanghai = true
 		F_State = fmt.Sprintf(template_1, date, zxMatch[1], gnMatch[1], shengMatch[1], shiMatch[1], shiMatch[2], xianMatch[1], xianMatch[2], tzMatch[1], xxMatch[1], jcMatch[1], ssMatch[1])
 	}
-	date := time.Now().Format("2006-01-02")
 	m := map[string]string{
 		"F_State":              base64.StdEncoding.EncodeToString([]byte(F_State)),
 		"__VIEWSTATE":          doc.Find("#__VIEWSTATE").AttrOr("value", ""),
@@ -175,7 +175,6 @@ func getViewParam(body io.Reader) map[string]string {
 		"p1$DaoXQLYCS":      "",  //旅游城市
 		"p1$Address2":       "中国",
 		"p1_SuiSMSM_Collapsed": "false",
-		"p1_Collapsed":         "false"
 	}
 	if shanghai {
 		m["p1$TongZWDLH"] = tzMatch[1]

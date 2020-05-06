43c425a8749e6c682f59e9bccd79e7336e943b32
Shengliang <cubercsl@163.com>
2020-05-06 13:49:52 +0800

    fix: https

--- const.go
+++ const.go
@@ -2,9 +2,9 @@ package main
 
 const (
 	loginURL     = "https://newsso.shu.edu.cn/login"
-	homeURL      = "http://selfreport.shu.edu.cn/Default.aspx"
-	dayReportURL = "http://selfreport.shu.edu.cn/DayReport.aspx"
-	historyURL   = "http://selfreport.shu.edu.cn/ViewDayReport.aspx?day=%v"
+	homeURL      = "https://selfreport.shu.edu.cn/Default.aspx"
+	dayReportURL = "https://selfreport.shu.edu.cn/DayReport.aspx"
+	historyURL   = "https://selfreport.shu.edu.cn/ViewDayReport.aspx?day=%v"
 )
 type Area uint
 const (
